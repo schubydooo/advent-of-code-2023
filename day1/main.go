20aa3223927511ae4a1d5688e47ae0bbc2ddbff1
Scott Schubert <scottzschubert@gmail.com>
2023-12-02 18:52:54 -0800

    data validation

--- day1/main.go
+++ day1/main.go
@@ -11,19 +11,10 @@ func main() {
     fmt.Println("Welcome to Advent of Code Day 1")
 
 	// Import file 
-	// calibration_values := readFile()
-	readFile()
+	calibration_values := readFile()
 
 	// Parse each line for 1st and last int 
-    // for _, line := range calibration_values {
-
-	// 	// Data validation
-	// 	if len(line[0]) < 1 {
-	// 		fmt.Println("Line has no values")
-	// 	}
-	// 	fmt.Println(len(line[0]))
-	// }
-
+	fmt.Println(calibration_values)
 
 	// Sum returned lines
 	// TODO
@@ -39,7 +30,7 @@ func findFirstNum(row string) int {
 	return 1
 }
 
-func readFile() {
+func readFile() []string {
 	// Open the CSV file
 	file, err := os.Open("calibration_values.csv")
 	if err != nil {
@@ -48,6 +39,8 @@ func readFile() {
 	defer file.Close()
 
 	// Copy the CSV to a local variable
+	data := make([]string, 1000) //1000 appears to be the expected input file len
+	i := 0
 	reader := csv.NewReader(file)
 	for {
 		record, err := reader.Read()
@@ -55,12 +48,22 @@ func readFile() {
 			if err == io.EOF {
 				break
 			}
-			fmt.Println("Error reading row: ", err)
-			return 
+			panic("Error reading row")
 		}
 
-		fmt.Println(record)
+		// Data validation
+		if len(record) > 1 {
+			panic("CSV has more than 1 value per row")
+		}
+		if len(record[0]) < 1 {
+			panic("Line has no values")
+		}
+
+		data[i] = record[0]
+		i ++
 	}
+
+	return data
 
 
 }
